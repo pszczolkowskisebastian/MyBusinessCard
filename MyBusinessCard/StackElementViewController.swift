fc790e9720e8fccea06d25c8133a19cf28dba4b5
pszczolkowskisebastian <pszczolkowskisebastian@gmail.com>
2017-05-17 21:46:16 +0200

    first part

---
+++ MyBusinessCard/StackElementViewController.swift
@@ -0,0 +1,47 @@
+//
+//  StackElementViewController.swift
+//  MyBusinessCard
+//
+//  Created by Sebastian Pszczółkowski on 17.05.2017.
+//  Copyright © 2017 Sebastian Pszczółkowski. All rights reserved.
+//
+
+import UIKit
+
+class StackElementViewController: UIViewController {
+
+    @IBOutlet weak var headerLabel: UILabel!
+
+    var headerString:String? {
+        didSet {
+            configureView()
+        }
+    }
+    
+    func configureView() {
+        headerLabel.text = headerString
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- MyBusinessCard/ViewController.swift
+++ MyBusinessCard/ViewController.swift
@@ -8,12 +8,104 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UICollisionBehaviorDelegate {
+    
+    let data = ["O mnie", "Czym się zajmuję", "Moje pasje"]
+    
+    
+    var views = [UIView]()
+    var animator:UIDynamicAnimator!
+    var gravity:UIGravityBehavior!
+    var snap:UISnapBehavior!
+    var previousTouchPoint:CGPoint!
+    var viewDragging = false
+    var viewPinned = false
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        animator = UIDynamicAnimator(referenceView: self.view)
+        gravity = UIGravityBehavior()
+        
+        animator.addBehavior(gravity)
+        gravity.magnitude = 4
+        
+        var offset:CGFloat = 250
+        
+        for i in 0 ... data.count - 1 {
+            if let view = addViewController(atOffset: offset, dataForVC: data[i] as AnyObject) {
+                views.append(view)
+                offset -= 50
+            }
+        }
+        
     }
+    
+    func addViewController (atOffset offset:CGFloat, dataForVC data:AnyObject?) -> UIView? {
+        
+        let frameForView = self.view.bounds.offsetBy(dx: 0, dy: self.view.bounds.size.height - offset)
+        
+        let storyBoard = UIStoryboard(name: "Main", bundle: Bundle.main)
+        
+        if let stackElementVC = storyBoard.instantiateViewController(withIdentifier: "StackElement") as? StackElementViewController {
+            
+            view.frame = frameForView
+            view.layer.cornerRadius = 5
+            view.layer.shadowOffset = CGSize(width: 2, height: 2)
+            view.layer.shadowColor = UIColor.black.cgColor
+            view.layer.shadowRadius = 3
+            view.layer.shadowOpacity = 0.5
+            
+            
+            if let headerStr = data as? String {
+                stackElementVC.headerString = headerStr
+            }
+            
+            
+            self.addChildViewController(stackElementVC)
+            self.view.addSubview(view)
+            stackElementVC.didMove(toParentViewController: self)
+            
+            let panGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(ViewController.handlePan(gestureRecoginzer:)))
+            view.addGestureRecognizer(panGestureRecognizer)
+            
+            let collision = UICollisionBehavior(items: [view])
+            collision.collisionDelegate = self
+            animator.addBehavior(collision)
+            
+            let boundary = view.frame.origin.y + view.frame.size.height
+            
+            // lower boundary
+            var boundaryStart = CGPoint(x: 0, y: boundary)
+            var boundaryEnd = CGPoint(x: self.view.bounds.size.width, y: boundary)
+            collision.addBoundary(withIdentifier: 1 as NSCopying, from: boundaryStart, to: boundaryEnd)
+            
+            
+            // upper boundary
+            boundaryStart = CGPoint(x: 0, y: 0)
+            boundaryEnd = CGPoint(x: self.view.bounds.size.width, y: 0)
+            collision.addBoundary(withIdentifier: 2 as NSCopying, from: boundaryStart, to: boundaryEnd)
+            
+            
+            gravity.addItem(view)
+            
+            
+            let itemBehavior = UIDynamicItemBehavior(items: [view])
+            animator.addBehavior(itemBehavior)
+            
+            return view
+            
+        }
+        
+        return nil
+        
+    }
+    
+    func handlePan (gestureRecoginzer:UIGestureRecognizer) {
+        
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
