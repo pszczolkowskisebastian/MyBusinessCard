71d077d054b37f9e2a5becab53ede9c321e8650c
pszczolkowskisebastian <pszczolkowskisebastian@gmail.com>
2017-05-17 20:32:03 +0200

    Initial Commit

---
+++ MyBusinessCard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyBusinessCard
+//
+//  Created by Sebastian Pszczółkowski on 17.05.2017.
+//  Copyright © 2017 Sebastian Pszczółkowski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
